700b458e19d5627f80c44852c7f3e1de86b5f724
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 00:41:02 -0500

    skeleton for forgot/password reset

---
+++ controllers/ResetPasswordCtrl.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+console.log('SessionsCtrl Loaded');
+
+router.post('/', function (req, res) {
+  // verify user email, create reset token & expiry date
+  // store in user model
+  // send reset email to user.email
+  // notify user that reset email will arrive shortly
+  res.status(200).send('success create reset tokens');
+});
+
+router.get('/reset/:token', function (req, res) {
+  // find user that has req.params.token
+  // if user found, redirect user to reset form
+  // return user in body, so token is available
+  // for password reset
+  res.status(200).send('success found reset token');
+});
+
+router.post('/reset/:token', function (req, res) {
+  // find user that has req.params.token
+  // verify the token is still valid
+  // clear reset token & expiry
+  // update with new passwords
+  // redirect user to login page
+  res.status(200).send('success updated password');
+});
+
+router.all('*', function (req, res) {
+  res.redirect('/');
+})
+
+module.exports = router;
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,12 @@
 var LandingPageCtrl = require('./LandingPageCtrl.js');
 var SessionsCtrl = require('./SessionsCtrl.js');
+var ResetPasswordCtrl = require('./ResetPasswordCtrl.js');
 
 module.exports = function (app) {
   console.log('Routes Loaded');
 
   app.use('/sessions', SessionsCtrl);
-
+  app.use('/forgotpassword', ResetPasswordCtrl);
   //Catchall Route for Angular App
   app.use('*', LandingPageCtrl);
 }
