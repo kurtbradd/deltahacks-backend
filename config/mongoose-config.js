b8f8108efbe3cc0aa623308bce00ed58584c7231
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 00:24:08 -0500

    connect to mongodb

---
+++ config/mongoose-config.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var secrets = require('./secrets.js');
+
+module.exports = function () {
+  mongoose.connect(secrets.db);
+
+  var connection = mongoose.connection;
+  connection.on('error', function () {
+    console.error('MongoDB Connection Error.');
+  });
+
+  connection.on('connected', function () {
+    console.log('Mongoose Connected');
+  })
+}
