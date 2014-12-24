c36500b5fff05ab49c5d044b65b0e8005763ff7d
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 17:13:04 -0500

    added skeleton method descriptions

--- controllers/SessionsCtrl.js
+++ controllers/SessionsCtrl.js
@@ -4,19 +4,25 @@ var router = express.Router();
 console.log('SessionsCtrl Loaded');
 
 router.post('/signup', function (req, res) {
+  // verify form fields
+  // create user model
+  // create verification token
+  // store verification token
+  // send verification email
+  // send jwt token & user model
   res.status(200).send('success signup');
 });
 
 router.post('/login', function (req, res) {
+  // verify user email and password
+  // create jwt token
+  // send jwt token and user model
   res.status(200).send('success login');
 });
 
 router.get('/logout', function (req, res) {
+  // expire jwt
   res.status(200).send('success logout');
 });
-
-router.all('*', function (req, res) {
-  res.redirect('/');
-})
 
 module.exports = router;
