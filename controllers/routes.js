31454f26c68158a4a8a7e50428368a251445cf5a
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 00:24:36 -0500

    included sessions route

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,10 @@
 var LandingPageCtrl = require('./LandingPageCtrl.js');
+var SessionsCtrl = require('./SessionsCtrl.js');
 
 module.exports = function (app) {
-  console.log('Routes Loded');
+  console.log('Routes Loaded');
+
+  app.use('/sessions', SessionsCtrl);
 
   //Catchall Route for Angular App
   app.use('*', LandingPageCtrl);
