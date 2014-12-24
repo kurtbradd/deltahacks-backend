2255940f043b1a7050b2ca3c3967c526b80281ac
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 17:15:14 -0500

    added new controller routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,12 +1,17 @@
 var LandingPageCtrl = require('./LandingPageCtrl.js');
 var SessionsCtrl = require('./SessionsCtrl.js');
 var ResetPasswordCtrl = require('./ResetPasswordCtrl.js');
+var ApplicationCtrl = require('./ApplicationCtrl.js');
+var AdminCtrl = require('./AdminCtrl.js');
 
 module.exports = function (app) {
   console.log('Routes Loaded');
 
   app.use('/sessions', SessionsCtrl);
   app.use('/forgotpassword', ResetPasswordCtrl);
+  app.use('/application', ApplicationCtrl);
+  app.use('/admin', AdminCtrl);
+
   //Catchall Route for Angular App
   app.use('*', LandingPageCtrl);
 }
