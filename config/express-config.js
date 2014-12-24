3bf51422e6a801336781fdcf5c05d601175fd08b
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-23 20:16:23 -0500

    created environment config files

---
+++ config/express-config.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+module.exports = function (app) {
+  // view engine setup
+  app.set('views', 'public/views');
+  app.engine('html', require('ejs').renderFile);
+  
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static('./public'));
+}
