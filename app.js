a9bd257f1e99bd8003f535eec5dfceff937c4a55
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-24 00:26:13 -0500

    require config files

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Connect MongoDB
+require('./config/mongoose-config.js')();
+
+// Require Models
+require('./models/models.js')();
+
 // Express Config
 require('./config/express-config.js')(app);
 
 //Load Routes
 require('./controllers/routes.js')(app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 //Error Handlers
 require('./config/error-handlers.js')(app);
