119fe4abb054a04b70dfa5b74fcad214ad89f01a
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-31 04:01:50 -0500

    log all errors, and send as JSON (caused rendering conflicts)

--- config/error-handlers.js
+++ config/error-handlers.js
@@ -1,21 +1,20 @@
 module.exports = function (app) {
+
   // development error handler
   // will print stacktrace
   if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
+      console.log(err);
+      res.status(err.status).send(err.message);
     });
   }
 
   // production error handler
   // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
+    console.log(err);
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
       message: err.message,
       error: {}
     });
