2e9499d1a7e87486835736ec3dd21cc8373f2aca
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-30 21:37:50 -0500

    added skeleton methods to dashboard and login ctrls

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,26 +2,28 @@ var module = angular.module('deltahacks.controllers', []);
 
 module.controller('LoginCtrl', ['$scope', function ($scope) {
   console.log('LoginCtrl loaded');
+  $scope.fields = {};
+  $scope.login = function () {
+    console.log('signup function called');
+    console.log($scope.fields);
+    // $state.go('dashboard')
+  };
 }])
 
 module.controller('DashboardCtrl', ['$scope', function ($scope) {
   console.log('DashboardCtrl loaded');
+  $scope.logout = function () {
+    // delete tokens
+    // $state.go('landingpage')
+  }
 }])
 
 module.controller('SignupCtrl', ['$scope', function ($scope) {
   console.log('SignupCtrl loaded');
-
-  $scope.fields = {
-    email:'',
-    password:'',
-    confirmPassword:'',
-    firstName:'',
-    lastName:'',
-    school:''
-  };
-
+  $scope.fields = {};
   $scope.signup = function () {
     console.log('signup function called');
     console.log($scope.fields);
+    // $state.go('dashboard')
   };
 }])
