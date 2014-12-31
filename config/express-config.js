c9c5b70f46df44dfec7fbf7ad6437b639efc64d7
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-30 20:47:14 -0500

    added favicon

--- config/express-config.js
+++ config/express-config.js
@@ -9,9 +9,7 @@ module.exports = function (app) {
   // view engine setup
   app.set('views', 'public/views');
   app.engine('html', require('ejs').renderFile);
-  
-  // uncomment after placing your favicon in /public
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(favicon('./public/favicon.ico'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
