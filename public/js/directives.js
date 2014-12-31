4bf5aaea7234fb1bda8ba3de1ddc63a34a096e3f
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-31 04:02:20 -0500

    cleaned up dom creation code

--- public/js/directives.js
+++ public/js/directives.js
@@ -6,10 +6,10 @@ app.directive('formField', function($compile) {
 		// template: '<div></div>',
 		link: function(scope, iElem, iAttrs) {				
 				var input = '<input class="form-control"' +
-				'name="' + iAttrs['bindTo'] + '" ' +
-				'placeholder="' + iAttrs['formTitle'] + '" ' +
-				'ng-model="' + iAttrs['bindTo'] + '" ' +
-				'required>';
+				'name=' + iAttrs['bindTo'] +
+				' placeholder=' + iAttrs['formTitle'] +
+				' ng-model=' + iAttrs['bindTo'] +
+				' required>';
 				var inputDiv = '<div class="col-sm-4">' + input + '</div>'
 				var label = '<label class="col-sm-2 control-label">' + iAttrs['formTitle'] + '</label>';
 				var formGroup =  '<div class="form-group">' + label + inputDiv + '</div>';
