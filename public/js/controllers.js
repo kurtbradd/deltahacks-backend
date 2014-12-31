86dd2a3cdfb3d93025e452da1cbbb46222ab91d5
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-31 04:05:18 -0500

    basic implementation of simple app features

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,29 +1,66 @@
 var module = angular.module('deltahacks.controllers', []);
 
-module.controller('LoginCtrl', ['$scope', function ($scope) {
+module.controller('LoginCtrl', ['$scope', 'UserService', '$state', 
+  function ($scope, UserService, $state) {
   console.log('LoginCtrl loaded');
   $scope.fields = {};
   $scope.login = function () {
-    console.log('signup function called');
-    console.log($scope.fields);
-    // $state.go('dashboard')
+    UserService.login($scope.fields.email, $scope.fields.password,
+      function (err) {
+        if (err) return console.log(err);
+        $state.go('dashboard');
+      })
   };
 }])
 
-module.controller('DashboardCtrl', ['$scope', function ($scope) {
-  console.log('DashboardCtrl loaded');
-  $scope.logout = function () {
-    // delete tokens
-    // $state.go('landingpage')
-  }
-}])
-
-module.controller('SignupCtrl', ['$scope', function ($scope) {
+module.controller('SignupCtrl', ['$scope', 'UserService', '$state', 
+  function ($scope, UserService, $state) {
   console.log('SignupCtrl loaded');
   $scope.fields = {};
+  $scope.error = '';
   $scope.signup = function () {
-    console.log('signup function called');
-    console.log($scope.fields);
-    // $state.go('dashboard')
+    if ($scope.password != $scope.confirmPassword) {
+      $scope.error = 'Passwords Do Not Match';
+      return;
+    }
+
+    UserService.signup($scope.fields,
+      function (err) {
+        if (err) return console.log(err);
+        $state.go('dashboard');
+      })
   };
+}])
+
+module.controller('DashboardCtrl', 
+  ['$scope', 'UserService', '$state', 'ApplicationService',
+  function ($scope, UserService, $state, ApplicationService) {
+  console.log('DashboardCtrl loaded');
+
+  $scope.getStatus = function () {
+    ApplicationService.getStatus()
+    .success(function (data) {
+      console.log(data);
+    })
+    .error(function (err) {
+      console.log(err)
+    });
+  }
+
+  $scope.updateStatus = function () {
+    ApplicationService.updateStatus($scope.statusUpdate,
+      function (err) {
+        if (err) return console.log(err);
+      })
+  }
+
+  $scope.logout = function () {
+    UserService.logout()
+    .success(function() {
+      $state.go('landingpage');
+    })
+    .error(function(err) {
+
+    });
+  }
 }])
