42b3769292e8e2ca66e9ecd65dd4d8a199dc3921
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-31 04:04:30 -0500

    use express JWT middleware on secured routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,14 +3,16 @@ var SessionsCtrl = require('./SessionsCtrl.js');
 var ResetPasswordCtrl = require('./ResetPasswordCtrl.js');
 var ApplicationCtrl = require('./ApplicationCtrl.js');
 var AdminCtrl = require('./AdminCtrl.js');
+var secret = require('../config/secrets.js').sessionSecret;
+var jwt = require('express-jwt');
 
 module.exports = function (app) {
   console.log('Routes Loaded');
 
   app.use('/sessions', SessionsCtrl);
   app.use('/forgotpassword', ResetPasswordCtrl);
-  app.use('/application', ApplicationCtrl);
-  app.use('/admin', AdminCtrl);
+  app.use('/application', jwt({secret:secret}), ApplicationCtrl);
+  app.use('/admin', jwt({secret:secret}), AdminCtrl);
 
   //Catchall Route for Angular App
   app.use('*', LandingPageCtrl);
